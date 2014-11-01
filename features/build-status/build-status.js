463b61612e17a8e728a2e10941c5a0b081fadd62
Xavier Boubert <xavier@boubert.fr>
2014-11-01 11:01:14 +0100

    refactor(build-status): remove useless encapsulation

--- features/build-status/build-status.js
+++ features/build-status/build-status.js
@@ -1,32 +1,27 @@
-(function() {
-  'use strict';
+'use strict';
 
-  var fs = require('fs-extra');
+var fs = require('fs-extra');
 
-  module.exports = new (function BuildStatus() {
+module.exports = new (function BuildStatus() {
 
-    var _this = this;
+  var _this = this;
 
-    this.STATUS = {
-      ok: 'ok',
-      error: 'error',
-      processing: 'processing'
-    };
+  this.STATUS = {
+    ok: 'ok',
+    error: 'error',
+    processing: 'processing'
+  };
 
-    this.status = function(status) {
-      if(!_this.STATUS[status]) {
-        return false;
-      }
+  this.status = function(status) {
+    if(!_this.STATUS[status]) {
+      return false;
+    }
 
-      if(fs.existsSync('status.svg')) {
-        fs.removeSync('status.svg');
-      }
+    fs.removeSync('status.svg');
 
-      fs.copySync(__dirname + '/' + status + '.svg', 'status.svg');
+    fs.copySync(__dirname + '/' + status + '.svg', 'status.svg');
 
-      return true;
-    };
-
-  })();
+    return true;
+  };
 
 })();
