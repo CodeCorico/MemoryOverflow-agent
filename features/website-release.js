203bd9743efa828c215da7354a2af07e33772557
Xavier Boubert <xavier@boubert.fr>
2017-10-30 23:26:53 +0100

    fix(website release): use the new website generator

--- features/website-release.js
+++ features/website-release.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var extend = require('extend'),
+    path = require('path'),
     fs = require('fs-extra'),
     cmd = require('child_process'),
     glob = require('glob'),
@@ -60,6 +61,7 @@ module.exports = function websiteRelease(config, callback) {
   config.MEMORYOVERFLOW_PATH = WORK_PATH + '/' + config.MEMORYOVERFLOW_PATH;
   config.WEBSITE_PATH = WORK_PATH + '/' + config.WEBSITE_PATH;
   config.THEMACHINE_PATH = config.MEMORYOVERFLOW_PATH + '/' + config.THEMACHINE_PATH;
+  config.WEBSITE_ABSOLUTE_PATH = path.join(__dirname, '..', config.WEBSITE_PATH);
 
   console.log('\ngit clone --depth 1 ' + config.MEMORYOVERFLOW_REPO + ' ' + config.MEMORYOVERFLOW_PATH + '...');
 
@@ -77,116 +79,97 @@ module.exports = function websiteRelease(config, callback) {
         return _error(error, callback);
       }
 
-      console.log('\nexecute The Machine...');
+      console.log('\nCreate the .env...');
 
-      cmd.exec('npm run-script generate', {
-        cwd: config.THEMACHINE_PATH
-      }, function(error) {
+      fs.writeFileSync(config.THEMACHINE_PATH + '/.env', 'WEBSITE_TARGET=' + config.WEBSITE_ABSOLUTE_PATH);
+
+      console.log('\ngit clone ' + config.WEBSITE_REPO + ' ' + config.WEBSITE_PATH + '...');
+
+      cmd.exec('git clone ' + config.WEBSITE_REPO + ' ' + config.WEBSITE_PATH, function(error) {
         if(error) {
           return _error(error, callback);
         }
 
-        console.log('\ngit clone ' + config.WEBSITE_REPO + ' ' + config.WEBSITE_PATH + '...');
+        console.log('\nexecute The Machine...');
 
-        cmd.exec('git clone ' + config.WEBSITE_REPO + ' ' + config.WEBSITE_PATH, function(error) {
+        cmd.exec('npm run generate', {
+          cwd: config.THEMACHINE_PATH
+        }, function(error) {
           if(error) {
             return _error(error, callback);
           }
 
-          console.log('\ncopy ' + config.MEMORYOVERFLOW_PATH + '/website' + ' ' + config.WEBSITE_PATH + '...');
+          console.log('\ngit config user.name "' + config.USER_AGENT + '"');
 
-          fs.copySync(config.WEBSITE_PATH + '/README.md', config.WEBSITE_PATH + '/README.tmp');
-
-          fs.copySync(config.MEMORYOVERFLOW_PATH + '/website', config.WEBSITE_PATH);
-
-          fs.move(config.WEBSITE_PATH + '/README.tmp', config.WEBSITE_PATH + '/README.md', {
-            clobber: true
+          cmd.exec('git config user.name "' + config.USER_AGENT + '"', {
+            cwd: config.WEBSITE_PATH
           }, function(error) {
             if(error) {
               return _error(error, callback);
             }
 
-            console.log('\nremove EJS files...');
-            var ejsFiles = glob.sync(config.WEBSITE_PATH + '/**/*.ejs' , {});
-            console.log(ejsFiles);
+            console.log('\ngit config user.email "' + config.USER_AGENT_EMAIL + '"');
 
-            for(var i = 0, len = ejsFiles.length; i < len; i++) {
-              fs.removeSync(ejsFiles[i]);
-            }
-
-            console.log('\ngit config user.name "' + config.USER_AGENT + '"');
-
-            cmd.exec('git config user.name "' + config.USER_AGENT + '"', {
+            cmd.exec('git config user.email "' + config.USER_AGENT_EMAIL + '"', {
               cwd: config.WEBSITE_PATH
             }, function(error) {
               if(error) {
                 return _error(error, callback);
               }
 
-              console.log('\ngit config user.email "' + config.USER_AGENT_EMAIL + '"');
+              console.log('\ngit add -A');
 
-              cmd.exec('git config user.email "' + config.USER_AGENT_EMAIL + '"', {
+              cmd.exec('git add -A', {
                 cwd: config.WEBSITE_PATH
-              }, function(error) {
+              }, function(error, stdout) {
                 if(error) {
                   return _error(error, callback);
                 }
 
-                console.log('\ngit add -A');
-
-                cmd.exec('git add -A', {
+                cmd.exec('git status', {
                   cwd: config.WEBSITE_PATH
                 }, function(error, stdout) {
                   if(error) {
                     return _error(error, callback);
                   }
 
-                  cmd.exec('git status', {
+                  var status = stdout.split('\n');
+                  if(status.length && status[1].trim() == 'nothing to commit, working directory clean') {
+                    console.log('NOTHING TO COMMIT');
+                    return _success(callback);
+                  }
+
+                  var commitAuthor = ' --author="' + config.USER_AGENT + ' <' + config.USER_AGENT_EMAIL + '>"',
+                      commitLabel = config.COMMIT_LABEL
+                        .replace('{commitID}', config.commitID)
+                        .replace('{commitUrl}', config.commitUrl)
+                        .split('\n')
+                        .map(function(line) {
+                          return ' -m "' + line + '"';
+                        })
+                        .join('');
+
+                  console.log('\ngit commit' + commitAuthor + commitLabel);
+
+                  cmd.exec('git commit' + commitAuthor + commitLabel, {
                     cwd: config.WEBSITE_PATH
-                  }, function(error, stdout) {
+                  }, function(error) {
                     if(error) {
                       return _error(error, callback);
                     }
 
-                    var status = stdout.split('\n');
-                    if(status.length && status[1].trim() == 'nothing to commit, working directory clean') {
-                      console.log('NOTHING TO COMMIT');
-                      return _success(callback);
-                    }
+                    var pushRepo = config.WEBSITE_REPO.replace('https://', 'https://' + config.USER_AGENT + ':' + config.SECRET + '@');
 
-                    var commitAuthor = ' --author="' + config.USER_AGENT + ' <' + config.USER_AGENT_EMAIL + '>"',
-                        commitLabel = config.COMMIT_LABEL
-                          .replace('{commitID}', config.commitID)
-                          .replace('{commitUrl}', config.commitUrl)
-                          .split('\n')
-                          .map(function(line) {
-                            return ' -m "' + line + '"';
-                          })
-                          .join('');
+                    console.log('\ngit push ' + pushRepo.replace(config.SECRET, 'SECRET') + ' gh-pages');
 
-                    console.log('\ngit commit' + commitAuthor + commitLabel);
-
-                    cmd.exec('git commit' + commitAuthor + commitLabel, {
+                    cmd.exec('git push ' + pushRepo + ' gh-pages', {
                       cwd: config.WEBSITE_PATH
                     }, function(error) {
                       if(error) {
                         return _error(error, callback);
                       }
 
-                      var pushRepo = config.WEBSITE_REPO.replace('https://', 'https://' + config.USER_AGENT + ':' + config.SECRET + '@');
-
-                      console.log('\ngit push ' + pushRepo.replace(config.SECRET, 'SECRET') + ' gh-pages');
-
-                      cmd.exec('git push ' + pushRepo + ' gh-pages', {
-                        cwd: config.WEBSITE_PATH
-                      }, function(error) {
-                        if(error) {
-                          return _error(error, callback);
-                        }
-
-                        return _success(callback);
-
-                      });
+                      return _success(callback);
                     });
                   });
                 });
